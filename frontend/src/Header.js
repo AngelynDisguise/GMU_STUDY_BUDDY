fff1f88e4a4654c841d1b6cc0fb265f422278965
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-02-17 14:11:11 -0500

    Add material ui dependencies

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import PersonIcon from '@mui/icons-material/Person';
+import MessageIcon from '@mui/icons-material/Message';
 
 /*Component returns JSX (html+css+js) */
 function Header() {
     return (
         <div>
+            <PersonIcon />
             <h2>Header</h2>
+            <MessageIcon />
         </div>
     )
 }
