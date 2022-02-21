3ef314ae7b8e2d423b80dfb087ef7caf2fe878c0
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-02-21 18:26:41 -0500

    fixed material-ui icons! (no compilation errors)

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
 /*Component returns JSX (html+css+js) */
 function Header() {
     return (
         <div className="header">
+            <PersonIcon />
+            <ForumIcon />
             <h2>Header</h2>
         </div>
     );
 }
-
+ 
 export default Header;
