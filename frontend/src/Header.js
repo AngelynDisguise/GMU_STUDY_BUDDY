5cdae35bd767a9b606bcc9dffc7e861478175e05
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-02-21 20:05:13 -0500

    Added style to Header.js

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
+
+import PatriotLogo from "./images/patriotLogo.png";
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+
+import "./Header.css";
 
 /*Component returns JSX (html+css+js) */
 function Header() {
     return (
         <div className="header">
-            <PersonIcon />
-            <ForumIcon />
-            <h2>Header</h2>
+            <PersonIcon className="navButton"/>
+            <img alt="Patriot Logo" className="patriotLogo" src={PatriotLogo} />
+            <ForumIcon className="navButton"/>
         </div>
     );
 }
