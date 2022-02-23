4f675282bcf7bde214dc2655836a428e83f00edc
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-02-23 16:07:42 -0500

    Add Login Page and install Auth0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 
 //components
+import Login from './components/Login';
 import Header from './components/Header';
 import Home from './components/Home';
 import ChatList from './components/ChatList';
@@ -12,6 +13,7 @@ import Chat from './components/Chat';
 
 //styles
 import './App.css';
+import './styles/Login.css';
 import './styles/Header.css';
 import './styles/Home.css';
 import './styles/ChatList.css';
@@ -23,9 +25,9 @@ function App() {
     <div className="App" >
       {/* Header */}
       <Header />
-
-      {/* Routes */}
-      <Routes>
+      <div className="wrapper">
+        {/* Routes */}
+        <Routes>
         {/* Chat list screen */}
         <Route path="/chatList" element={<ChatList />} />
 
@@ -35,6 +37,7 @@ function App() {
         {/* Home: Login + Study Buddy Card */}
         <Route path="/" element={<Home />} />
       </Routes>
+      </div>
     </div>
   );
 }
