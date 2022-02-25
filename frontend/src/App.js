f4aebe42b59f3ed6f7180438a4e4dd7da68d271a
Ian Boyd <iboyd4@gmu.edu>
2022-02-24 22:36:54 -0500

    Implemented a simple header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
 
-import Header from './Header';
+//components
+import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
+import ChatList from './components/ChatList';
+import Chat from './components/Chat';
 
+//styles
 import './App.css';
+import './styles/Login.css';
+import './styles/Header.css';
+import './styles/Home.css';
+import './styles/ChatList.css';
+import './styles/Chat.css';
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
+  const [token, setToken] = useState();
 
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return ( 
+    <div className="App" >
+      {/* Header */}
+      <Header />
+      <div className="wrapper">
+        
+        {/* Routes */}
+        <Routes>
+        
+          {/* Chat list screen */}
+          <Route path="/chatList" element={<ChatList />} />
+
+          {/* Individual chat screen */}
+          <Route path="/chat" element={<Chat />} />
+          
+          {/* Home: Login + Study Buddy Card */}
+          <Route path="/" element={<Home />} />
+      </Routes>
+      </div>
     </div>
   );
 }
