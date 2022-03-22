c63b241393815c6e1a4e8fd4da84f057e56f0b77
mparikh3939 <mparikh3939@gmail.com>
2022-03-22 17:51:01 -0400

    Added and populated the users and user info table

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,6 +1,17 @@
 modules.exports = (sequelize, DataTypes) => {
     const users = sequelize.define("users", {
-
+        email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
     });
     return users;
 };
