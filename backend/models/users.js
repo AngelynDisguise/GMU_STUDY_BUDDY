2b905db9c6d41e3bf023b6ab38f8dccbfd65e956
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-03-26 17:22:37 -0400

    Comment out sequelize-file & simplified user data

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,17 +1,48 @@
+// import { picture } from './attachments';
 modules.exports = (sequelize, DataTypes) => {
-    const users = sequelize.define("users", {
+    const userinfo = sequelize.define("userinfo", {
+        /****************** PRIVATE INFO *****************/
+        /**
+         * Check if includes “@gmu.edu” substring
+         */
         email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		username: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        /**
+         * Date format: [month/day/year]
+         * Index 1 = month
+         * Index 2 = day
+         * Index 3 = year
+         * 
+         * Prop not shown on matches list; shown as age only
+         */
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        /****************** PUBLIC INFO *****************/
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        /**
+         * Limited to Male/Female (for now)
+         * Make selector/drop-down options for this
+         */
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        /**
+         * Limited to one major (for now)
+         */
+        major: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     });
-    return users;
+    // picture.addTo(userinfo);
+    return userinfo;
 };
