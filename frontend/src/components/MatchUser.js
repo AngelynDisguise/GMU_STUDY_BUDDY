11f06cd0c14fdf4ad596bd32c975ad1967642664
iboyd4 <93687474+iboyd4@users.noreply.github.com>
2022-04-14 16:45:24 -0400

    added divs to matchuser page, styled a few things

--- frontend/src/components/MatchUser.js
+++ frontend/src/components/MatchUser.js
@@ -7,8 +7,25 @@ function MatchUser() {
     
     return(
         <div className='wrapper'>
-            <h1>User info</h1>
-            <h2>{user}</h2>
+            <div className='mheader'>
+                <h1>User info</h1>
+            </div>
+            
+            <div className='name'>
+                <h2>Name: {user}</h2>
+            </div>
+
+            <div className='gender'>
+                <h3>Gender:</h3>
+            </div>
+            
+            <div className='major'>
+                <h3>Major:</h3>
+            </div>
+            
+            <div className='age'>
+                <h3>Age:</h3>
+            </div>
         </div>
     );
 }
