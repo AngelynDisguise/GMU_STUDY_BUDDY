5472b1cffcc5199c1afab6d4cb05a5294f9a6dcd
mparikh3939 <mparikh3939@gmail.com>
2022-04-15 21:10:37 -0400

    Copied the code off of the main

--- backend/routes/StudyBuddyList.js
+++ backend/routes/StudyBuddyList.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { StudyBuddyList: StudyBuddy } = require("../models");
+const { StudyBuddy } = require("../models");
 
 //List all users
 router.get("/", async(req, res) => {
@@ -10,24 +10,33 @@ router.get("/", async(req, res) => {
 
 //Added a new user
 router.post("/add", async(req, res) => {
-    const {email} = req.body; //get body of data being pass in
-    try {
-        const newUser = await StudyBuddy.create({email : email});
-        res.json(newUser);
-    } catch (err) {
-        res.status(500).send(err); 
-    }
-});
-
-//Remove an existing user
-router.post("/remove", async(req, res, next) => {
-    const {email} = req.body; //get body of data being pass in
+    const { email } = req.body; //get body of data being pass in
     const user = await StudyBuddy.findOne({
         where: {
             email: email,
         },
     });
-    if (user !== null) {
+    if (user) {
+        res.json("User already exists!");
+    } else {
+        try {
+            const newUser = await StudyBuddy.create({ email: email });
+            res.json(newUser);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+});
+
+//Remove an existing user
+router.post("/remove", async(req, res, next) => {
+    const { email } = req.body; //get body of data being pass in
+    const user = await StudyBuddy.findOne({
+        where: {
+            email: email,
+        },
+    });
+    if (user) {
         StudyBuddy.destroy({
             where: {
                 email: email,
@@ -38,30 +47,5 @@ router.post("/remove", async(req, res, next) => {
         res.json("User not found!");
     }
 });
-
-// //Remove a user
-// router.post("/update", async(req, res) => {
-//     // const user = await Users.findOne({
-//     //     where: {
-//     //         email: email,
-//     //     },
-//     // });
-//     // if (user !== null) {
-//     //     bycrypt.hash(password, 10).then(async(hash) => {
-//     //         try {
-//     //             const newUser = await Users.create({
-//     //                 email: email,
-//     //                 password: hash,
-//     //             });
-//     //             res.json(newUser);
-//     //         } catch (err) {
-//     //             res.status(500).send(err);
-//     //         }
-//     //     });
-//     // } else {
-//     //     res.json("User already exists.");
-//     // }
-// });
-
 //middleware for server.js
 module.exports = router;
