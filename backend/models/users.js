4a2460cde850a7dfd4417284ca54d3912a5cc916
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-04-17 20:37:21 -0400

    Remove study buddy and match list from routes, fix get requests

--- backend/models/users.js
+++ backend/models/users.js
@@ -13,6 +13,8 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         },
 
+
+
         /****************** PUBLIC INFO *****************/
         firstName: {
             type: DataTypes.STRING,
