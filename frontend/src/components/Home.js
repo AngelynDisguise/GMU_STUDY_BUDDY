340d5648691f3426e553b11b6fdbc733043f2800
iboyd4 <93687474+iboyd4@users.noreply.github.com>
2022-04-21 17:37:04 -0400

    Styled some things, added borders to various headings

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -43,7 +43,7 @@ function Home() {
   return (
     <div className='home'>
       <div className='home-header'>
-        <h1>Home!</h1>
+        <h2>Home</h2>
       </div>
       
       <div className='cardsContainer'>
